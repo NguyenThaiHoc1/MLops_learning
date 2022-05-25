fb9f624882cc6b02b989839ea61f24619cd9f222
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2022-05-25 18:38:35 +0700

    adding training for spark

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,16 @@
-from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
-
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.feature import HashingTF, Tokenizer
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("PiplineBasic").getOrCreate()
+
+training = spark.createDataFrame([
+    (0, "a b c d e spark", 1.0),
+    (1, "b d", 0.0),
+    (2, "spark f g h", 1.0),
+    (3, "hadoop mapreduce", 0.0)
+], ["id", "text", "label"])
 
 # prepare training documents from a list of (id, text, label) tuples.
 
@@ -12,3 +18,5 @@ tokenizer = Tokenizer(inputCol="text", outputCol="words")
 hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="features")
 lr = LogisticRegression(maxIter=10, regParam=0.001)
 pipeline = Pipeline(stages=[tokenizer, hashingTF, lr])
+
+model = pipeline.fit(training)
